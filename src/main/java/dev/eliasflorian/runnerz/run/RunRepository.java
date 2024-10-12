2aabccdcae9232359a8ad82e144196d01c4ccd3b
EliasFlorian <elias.damisch@gmail.com>
2024-10-12 16:39:10 +0200

    added RunController + RunRepo

---
+++ src/main/java/dev/eliasflorian/runnerz/run/RunRepository.java
@@ -0,0 +1,47 @@
+package dev.eliasflorian.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream().filter(run -> run.id() == id).findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if(existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run( 1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 4, Location.INDOOR));
+
+        runs.add(new Run( 2, "Evening Run", LocalDateTime.now(), LocalDateTime.now().plusHours(2), 5, Location.OUTDOOR));
+    }
+
+}
