013fd151fdb1a05d45077b8ac76044dd22653460
EliasFlorian <elias.damisch@gmail.com>
2024-10-18 16:34:55 +0200

    added postgres db / crud

--- src/main/java/dev/eliasflorian/runnerz/run/Run.java
+++ src/main/java/dev/eliasflorian/runnerz/run/Run.java
@@ -4,10 +4,13 @@ package dev.eliasflorian.runnerz.run;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
 public record Run(
+        @Id
         Integer id,
         @NotEmpty
         String title,
@@ -15,7 +18,9 @@ public record Run(
         LocalDateTime completedOn,
         @Positive
         Integer kilometers,
-        Location location
+        Location location,
+        @Version
+        Integer version
 ) {
 
     public Run {
